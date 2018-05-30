84490dcdaea280b29aa4e58794d6a6a643e654f6
kaa13051996 <Krupina.Alisa@gmail.com>
2018-05-31 01:56:52 +0700

    Update

--- dichotomy.py
+++ dichotomy.py
@@ -1,6 +1,7 @@
 # from random import uniform
 from math import fabs
 from time import time
+
 # var5
 start_time = time()
 a, b = 0, 3
@@ -8,18 +9,20 @@ epsilon = 0.001
 # delta = uniform(0, epsilon)
 delta = 7.110642666101863e-05
 
-def f(x):
-    return 20*(x-1)**2+95
 
-while (fabs(b-a) >= epsilon):
-    x_1 = (a + b)/2 - delta
-    x_2 = (a + b)/2 + delta
+def f(x):
+    return 20 * (x - 1) ** 2 + 95
+
+
+while (fabs(b - a) >= epsilon):
+    x_1 = (a + b) / 2 - delta
+    x_2 = (a + b) / 2 + delta
     fun_1, fun_2 = f(x_1), f(x_2)
-    if fun_2>fun_1:
+    if fun_2 > fun_1:
         a, b = a, x_2
     else:
         a, b = x_1, b
 
 print('a={0}, b={1}'.format(a, b))
-print('Min: ',(a+b)/2)
-print("--- %s seconds ---" % (time() - start_time))
+print('Min: ', (a + b) / 2)
+print("--- {0} seconds ---".format(time() - start_time))
--- gradient_descent.py
+++ gradient_descent.py
@@ -1,10 +1,9 @@
 from sympy import *
 from time import time
-
 start_time = time()
 
 def f(x_1, x_2):
-    return pow(x_1,2)+2*pow(x_2,2)
+    return pow(x_1, 2) + 2 * pow(x_2, 2)
 
 x_1, x_2 = symbols('x_1 x_2')
 dif = {}
@@ -14,20 +13,21 @@ print('Производные функции {0}: {1}'.format(f(x_1, x_2), dif))
 
 x_begin = [5, 4, f(5, 4)]
 x_mass = {0: x_begin}
-t = [0.001]
+t = [1]
+epsilon = 0.001
+max_iter = 1000
 print('t = {0}'.format(t[0]))
 
-for i in range(1, 5):
+count = 2
+for i in range(1, count):
     a = round(x_mass[i - 1][0] - t[0] * dif[0]._args[0] * x_mass[i - 1][0], 4)
     b = round(x_mass[i - 1][1] - t[0] * dif[1]._args[0] * x_mass[i - 1][1], 4)
-    fun = round(pow(a, 2) + 2 * pow(b, 2), 4)
-    x_mass[i] = [a, b, fun]
+    var = round(dif[0]._args[0] * a + dif[1]._args[0] * b)
+    x_mass[i] = [a, b, var]
+    if (var < epsilon) or count == max_iter:
+        break
+    count += 1
+
 print(x_mass)
 
-print("--- %s seconds ---" % (time() - start_time))
-
-
-
-
-
-
+print("--- {0} seconds ---".format(time() - start_time))
